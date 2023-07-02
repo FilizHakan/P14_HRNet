f0c0242e8a57d350c6b83dc02ec9bdc7e90989a2
Filiz <filizhakan13@gmail.com>
2023-07-02 11:06:29 +0200

    Add build

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,38 +1,27 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-import Logo from "../assets/HRNetWholeLogo.png";
+import Logo from '../assets/HRNetWholeLogo.png'
 
 /**
  * @name Error404
  * @dscription create the 404 error message component
  * @returns {JSX.Element}
  */
-export default function Error404() 
-{
+export default function Error404() {
   return (
-      <div>
-        <p className="errorTitle">404</p>
+    <div>
+      <p className="errorTitle">404</p>
 
-        <p 
-          className="errorContent"
-        >
-          Oops! The page you have requested does not exist.
-        </p>
+      <p className="errorContent">Oops! The page you have requested does not exist.</p>
 
-        <NavLink 
-          to={"/"} 
-          className="errorLink">
-            Return to the home page
-        </NavLink>
+      <NavLink to={'/'} className="errorLink">
+        Return to the home page
+      </NavLink>
 
-        <div className="logo404">
-          <img
-            src={Logo}
-            className="errorIcon"
-            alt="HRNet Logo"
-          />
-        </div>
-      </div>  
-          )
-};
+      <div className="logo404">
+        <img src={Logo} className="errorIcon" alt="HRNet Logo" />
+      </div>
+    </div>
+  )
+}
